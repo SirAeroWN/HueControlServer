04b33f58dfd84c5a51061a538893f3071250889c
norv <sicklyserver@gmail.com>
2026-01-01 22:09:56 -0500

    Fix quote escapes; cleanup

--- HueGoodNightCommand/Program.cs
+++ HueGoodNightCommand/Program.cs
@@ -9,13 +9,12 @@ namespace HueGoodNightCommand
 {
     internal class Program
     {
+        // Not used?
         static async Task<int> Main(string bridgeIp, string key)
         {
             // create the api object
             var localHueApi = new LocalHueApi(bridgeIp, key);
             Bedroom bedroom = new Bedroom(localHueApi);
-
-            Task fanTask = TurnOnFan();
 
             await bedroom.SetScene("To Bed");
 
@@ -30,53 +29,8 @@ namespace HueGoodNightCommand
 
             await bedroom.TurnLightsOff();
 
-            await fanTask;
-
             await Console.Out.WriteLineAsync("done");
             return 0;
-        }
-
-        private static async Task TurnOnFan()
-        {
-            string script = """
-            import broadlink
-            device = broadlink.hello('192.168.1.211')
-            device.auth()
-            onOffPacket = b"&\x00Z\x00\x93\x93\x111\x102\x110\x112\x101\x111\x10T\x0f2\x110\x11S\x102\x11R\x110\x112\x11R\x110\x112\x92\x94\x102\x101\x111\x111\x111\x102\x10S\x110\x112\x10S\x110\x11R\x112\x101\x11R\x112\x0f\x00\x01\xba\x00\x01&K\x10\x00\x06\x81\x00\x01'J\x11\x00\r\x05"
-            speedPacket = b"&\x00Z\x00\x93\x94\x102\x101\x111\x111\x110\x112\x10S\x101\x112\x101\x11R\x112\x0f2\x110\x11S\x10T\x0f2\x93\x93\x112\x101\x111\x102\x110\x111\x11S\x101\x111\x111\x11R\x111\x111\x102\x10S\x11R\x11\x00\x01\xb9\x00\x01&K\x11\x00\x06\x81\x00\x01'J\x10\x00\r\x05"
-            device.send_data(onOffPacket)
-            device.send_data(speedPacket)
-            """;
-            script = script.Split('\n').Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x)).Aggregate((x, y) => x + "; " + y);
-            Process p = new Process();
-            ProcessStartInfo psi = new ProcessStartInfo()
-            {
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                FileName = "python3",
-                Arguments = $"-c script"
-            };
-            p.StartInfo = psi;
-            p.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
-            p.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
-
-            await Console.Out.WriteLineAsync("Starting script");
-
-            p.Start();
-            p.BeginOutputReadLine();
-            p.BeginErrorReadLine();
-
-            await p.WaitForExitAsync();
-            await Console.Out.WriteLineAsync($"Script exited with {p.ExitCode}");
-            return;
-        }
-
-        static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
-        {
-            //* Do your stuff with the output (write to console/log/StringBuilder)
-            Console.WriteLine(outLine.Data);
         }
     }
 }
